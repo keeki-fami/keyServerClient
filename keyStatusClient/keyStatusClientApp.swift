7c1f8109541dc2aa6456471f477277836480959d
TowaSakurada <towatowa3939@icloud.com>
2026-03-27 11:14:20 +0900

    Initial Commit

---
+++ keyStatusClient/keyStatusClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  keyStatusClientApp.swift
+//  keyStatusClient
+//
+//  Created by 櫻田聖和 on 2026/03/27.
+//
+
+import SwiftUI
+
+@main
+struct keyStatusClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
